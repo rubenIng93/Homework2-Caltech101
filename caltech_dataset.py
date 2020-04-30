97a4499c1a8d812a1989257162bb31196bac4070
rubenIng93 <48275113+rubenIng93@users.noreply.github.com>
2020-04-30 17:53:10 +0200

    fixed errors

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -51,7 +51,7 @@ class Caltech(VisionDataset):
         img_lab = {} # dictionary k=index v=label
         for image in split_array:
             if image.split('/')[0].find('BACKGROUND') < 0:# filter that removes BACKGROUND 
-                rgb = pil_loader('/'+root+'/'+image.strip()) # e.g. Caltech101/101_ObjectCategories/accordion/image_0002.jpg
+                rgb = pil_loader(root+'/'+image.strip()) # e.g. Caltech101/101_ObjectCategories/accordion/image_0002.jpg
                 images[count] = rgb
                 img_lab[count] = image.split('/')[0]
                 count += 1
